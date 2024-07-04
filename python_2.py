d6560bebae97d7a6e7d7585ca9b064b5257846ce
Sunshine-Python <carena.wilson@outlook.com>
2024-07-04 12:54:10 +0200

    Update python_2.py

--- python_2.py
+++ python_2.py
@@ -52,8 +52,8 @@ def buy_and_hold_viz():
     x = np.arange(100)
     y = np.cumsum(np.random.randn(100)) + 100
     fig = go.Figure(go.Scatter(x=x, y=y, mode='lines', name='Stock Price'))
-    fig.update_layout(title='Buy and Hold Visualization', xaxis_title='Time', yaxis_title='Price', height=300, plot_bgcolor='white',
-        paper_bgcolor='white', margin=dict(l=30, r=30))
+    fig.update_layout(title='Buy and Hold Visualization', xaxis_title='Time', yaxis_title='Price', height=300, plot_bgcolor='#f2f2f2',
+        paper_bgcolor='#f2f2f2', margin=dict(l=30, r=30))
     st.plotly_chart(fig)
 
 # SMA Cross Strategy
